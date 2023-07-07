e09a4b98dcbf7931dd425b1cee704398c0645d39
Nazaninhk <121026788+Nazaninhk@users.noreply.github.com>
2023-07-07 13:48:51 -0700

    Showing the current tempreture

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function displayTempreture(response) {
+  let tempretureElement = document.querySelector("#tempreture");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  tempretureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "a96b8e42edf83dc1f3db15911c229c79";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTempreture);
